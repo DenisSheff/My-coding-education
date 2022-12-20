93594fb7a7a38c14bcedb3a6dc2299e3344fd328
Denis Shevchenko <denis_sheff@icloud.com>
2022-12-20 14:39:08 +0900

    New tasks added

---
+++ python_gb/Practice/discussion_class_2/class_two.py
@@ -0,0 +1,13 @@
+x = 0, 1
+y = 0, 1
+z = 0, 1
+print(bool(234))
+print(bool(452352345))
+print(bool(4564536))
+print(bool(54634563))
+print(bool(3563456))
+print(bool(None))
+print(bool(''))
+print(bool([]))
+print(bool({}))
+print(bool(()))
