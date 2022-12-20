b1575e61a8efd0aef713fff14cc81591bc234186
Denis Shevchenko <denis_sheff@icloud.com>
2022-12-20 15:03:07 +0900

    First task done

--- python_gb/Practice/discussion_class_2/task_one.py
+++ python_gb/Practice/discussion_class_2/task_one.py
@@ -1,7 +1,17 @@
 # 1. Напишите программу, которая принимает на вход число N и выдаёт последовательность из N членов.
 # *Пример:*
 # - Для N = 5: 1, -3, 9, -27, 81
+from os import *
+from random import *
+
+system('cls')
+print()
+num = int(input("Enter a value: "))
+
+for _ in range(num):
+    print(randrange(-100, 100), end=", ")
 
 
-num = int(input())
+
+
 
