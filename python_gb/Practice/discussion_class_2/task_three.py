7d2781bb6bfd4cf1f0769552dc4185a056cee6b0
Denis Shevchenko <denis_sheff@icloud.com>
2022-12-20 19:53:25 +0900

    Third task done 5

--- python_gb/Practice/discussion_class_2/task_three.py
+++ python_gb/Practice/discussion_class_2/task_three.py
@@ -15,12 +15,6 @@ str_2 = input("Enter value of the second string: ")
 
 counter = 0
 for i in range(len(str_1) - len(str_2)):  # проходим по всей строке по индексам
-    flag = False
-    if str_2[0] == str_1[i]:  # если находим соответствие то проверяем
-        flag = True
-        for j in range(1, len(str_2)):  # проверяем вторую строку
-            if str_2[j] != str_1[i + j]:  # если они не равны
-                flag = False
-        if flag:
-            counter += 1
+    if str_2 != str_1[i: i + len(str_2)]:  # если они не равны
+        counter += 1
 print(f"Second string is located in the first one {counter} times.")
