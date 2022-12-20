89d2471639619f19a54bf02713d272be5a0e8556
Denis Shevchenko <denis_sheff@icloud.com>
2022-12-20 16:07:48 +0900

    Third task done

--- python_gb/Practice/discussion_class_2/task_three.py
+++ python_gb/Practice/discussion_class_2/task_three.py
@@ -1,3 +1,15 @@
 # Напишите программу, в которой пользователь будет задавать две строки,
 # а программа - определять количество вхождений одной строки в другой.
 
+from os import *
+system('cls')
+
+str_1 = input("Enter value of the first string: ")
+str_2 = input("Enter value of the second string: ")
+
+# count = str_1.lower().count(str_2.lower())
+
+# print(count)
+
+print(str_1.lower().count(str_2.lower()))
+# метод count() является встроенным методом строки который считает количество вхождений в строку
