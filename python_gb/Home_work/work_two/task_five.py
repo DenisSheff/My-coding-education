c5d31eac81fb0f4229131292343d5c4aa5a6387c
Denis Shevchenko <denis_sheff@icloud.com>
2022-12-22 09:47:17 +0900

    Homewrok fifth task

--- python_gb/Home_work/work_two/task_five.py
+++ python_gb/Home_work/work_two/task_five.py
@@ -5,11 +5,19 @@ number = int(input("Enter an amount of elements in the list: "))
 new_list = []
 
 for i in range(number):
-    new_list.append(randint(-100, 100))
+    new_list.append(randint(-number, number))
 
 print(f"Original list is {new_list}.")
 # shuffle(new_list)
 # print(f"Shuffled list is {new_list}.")
-for _ in range(number):  # Выдает более рандомные значения при перемешевании элемента
-    shuffle(new_list)
-print(f"Shuffled list is {new_list}.")
+
+# Выдает более рандомные значения при перемешевании элемента
+# Через встроенную функцию
+# for _ in range(number):
+#     shuffle(new_list)
+# print(f"Shuffled list is {new_list}.")
+
+for element in range(number - 1):
+    i = randint(-number, number)
+    new_list[i], new_list[element] = new_list[element], new_list[i]
+print(new_list)
