98e2d5b8f71d4d6607c8312ddb05bb4930f6f674
Denis Shevchenko <denis_sheff@icloud.com>
2022-12-24 12:26:36 +0900

    1

--- python_gb/Practice/discussion_class_3/task_one.py
+++ python_gb/Practice/discussion_class_3/task_one.py
@@ -3,9 +3,22 @@
 # псевдослучайных чисел.
 
 from time import *
+from math import *
 
 random_number = time()
 
 print(random_number)
 print(int(round(random_number % 1 * 1000, 0)))
+
+def square(a):
+    return a * 2
+
+
+def rect(a, b):
+    return a * b
+
+
+def circle(r):
+    return pi * pow(r, 2)
+
 
