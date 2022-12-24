91afc3bb7a123f4a0375c87921029ed6c782f462
Denis Shevchenko <denis_sheff@icloud.com>
2022-12-24 12:18:12 +0900

    Task one. Random number

---
+++ python_gb/Practice/discussion_class_3/task_one.py
@@ -0,0 +1,11 @@
+# 1. Реализуйте алгоритм задания случайных чисел
+# без использования встроенного генератора
+# псевдослучайных чисел.
+
+from time import *
+
+random_number = time()
+
+print(random_number)
+print(int(round(random_number % 1 * 1000, 0)))
+
