d7d7e47501dbc7956c3195c67f3a478f128a70c9
Denis Shevchenko <denisshevchenko@Deniss-MacBook-Air.local>
2022-12-30 22:27:15 +0900

    Second task is done

---
+++ python_gb/Home_work/work_four/task_two.py
@@ -0,0 +1,17 @@
+# 2. Задайте натуральное число N. 
+# Напишите программу, которая составит список простых множителей числа N.
+
+
+number = int(input("Enter a number: "))
+simple_number = 2  
+my_list = []
+recent_number = number
+
+while simple_number <= number:
+    if number % simple_number == 0:
+        my_list.append(simple_number)
+        number //= simple_number
+        simple_number = 2
+    else:
+        simple_number += 1
+print(f"Order of simple multiple numbers of number {recent_number} is listed here: {my_list}")
