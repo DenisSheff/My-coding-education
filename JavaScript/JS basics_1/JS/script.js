e982d8ad73c6bb4f4c00e67636abbd8e5f7fa93f
Denis Shevchenko <denisshevchenko@Deniss-MacBook-Air.local>
2023-01-12 11:56:16 +0900

    ../

---
+++ JavaScript/JS basics_1/JS/script.js
@@ -0,0 +1,14 @@
+
+// alert('Hello everyone!:)'); функция вывода сообщения на экран
+// let number = 5; // создание переменной через ключевое слово "let"
+// alert(number);
+// console.log(number);
+// number = 10
+// console.log(number);
+// for (let index = 0; index < 100; index++) {
+//   number++;
+//   console.log(number);
+// }
+
+
+
