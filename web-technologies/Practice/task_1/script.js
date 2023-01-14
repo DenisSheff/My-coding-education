2990cc4006dd124a18d3a9cc183115f43445cc68
Denis Shevchenko <denisshevchenko@Deniss-MacBook-Air.local>
2023-01-14 12:00:28 +0900

    First day

--- web-technologies/Practice/task_1/script.js
+++ web-technologies/Practice/task_1/script.js
@@ -1,5 +1,40 @@
-"use strict"
+// "use strict"
 
-let num1 = Number(prompt('Enter a first value: '));
-let num2 = Number(prompt('Enter a second value: '));
-alert(`Sum of the given values: ${num1} and ${num2} is ${num1 + num2}`);
+// let num1 = Number(prompt('Enter a first value: '));
+// let num2 = Number(prompt('Enter a second value: '));
+// alert(`Sum of the given values: ${num1} and ${num2} is ${num1 + num2}`);
+
+// let message = confirm('How is it going?');
+// if (message) {
+//   alert("We're coming!");
+// } 
+// else {
+//   alert("You can do it!");
+// }
+
+// switch (prompt()) {
+//   case "Мандарины":
+//       alert('Мандарины стоят 100 руб/кг.');
+//       break;
+//   case "Бананы": 
+//   case "Груши":
+//     alert('Бананы и груши стоят 70 руб/кг.');
+//     break;
+//   default:
+//     alert('Нет такого продукта.');
+// }
+
+
+ 
+// function getMaxEvenElement(arr) {
+//   let result = arr[0];
+//   for (let i = 2; i < arr.length; i += 2) {
+//     if (result < arr[i]) {
+//       result = arr[i];
+//     }
+//   }
+//   return result;
+// }
+ 
+// console.log(getMaxEvenElement([5, 7, -1, 12, 3, 0])); // 5
+// console.log(getMaxEvenElement([4, -12, 29, 6, 31, 92, -50])); // 31
